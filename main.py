fe502b430d6d96b8d956ea842977f53aa0d21455
RIDERIUS <riderius.help@gmail.com>
2021-04-21 22:30:19 +0300

    Code cleanup

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 
 import sys
 import asyncio
-import sqlite3 as sql
 import discord
 from loguru import logger
 from settings import TOKEN
 
-__version__ = "0.5-dev3"
+__version__ = "0.5-dev4"
 __author__ = "RIDERIUS"
 
 # pylint settings:
@@ -27,69 +26,11 @@ logger.add(
 client = discord.Client()
 
 
-@logger.catch
-def database(accound_id: int, time_in_voice: float):
-    """
-    database - function for working with a database
-
-    Args:
-        accound_id (int): this is the variable that stores the ID of the Discord account
-        time_in_voice (float): it is a variable that stores the number of minutes in voice channels
-    """
-
-    connection = sql.connect("database.db")
-
-    cursor = connection.cursor()
-
-    try:
-        cursor.execute("CREATE TABLE stocks (id TEXT, time_in_voice REAL)")
-    except sql.OperationalError:
-        pass
-
-    data = (round(time_in_voice / 60, 1), accound_id)
-
-    cursor.execute("Update stocks set time_in_voice = ? where id = ?", data)
-
-    # accound_id = input('Enter the account id: ')
-    # time_in_voice = input('Enter the time in voice: ')
-
-    # cursor.execute(
-    #     f"INSERT INTO stocks VALUES ('{accound_id}','{time_in_voice}')")
-
-    connection.commit()
-
-    cursor.execute("SELECT * FROM `stocks`")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row[0], row[1])
-
-    connection.close()
-
-
-@logger.catch
-@client.event
-async def on_member_join(member):
-    """This feature is only triggered if a member joins the server"""
-
-    connection = sql.connect("database.db")
-
-    cursor = connection.cursor()
-
-    try:
-        cursor.execute("CREATE TABLE stocks (id TEXT, time_in_voice REAL)")
-    except sql.OperationalError:
-        pass
-
-    cursor.execute(f"INSERT INTO stocks VALUES ('{member.id}','0.0')")
-
-    connection.commit()
-
-    cursor.execute("SELECT * FROM `stocks`")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row[0], row[1])
-
-    connection.close()
+# @logger.catch
+# @client.event
+# async def on_member_join(member):
+#     """This feature is only triggered if a member joins the server"""
+#     pass
 
 
 @logger.catch
@@ -161,31 +102,6 @@ async def on_message(message) -> None:
         await editon_message.edit(content=end_message)
         logger.info("End message: " + end_message)
 
-    # Command voice
-    if message.content.startswith("!voice"):
-        # user_id = message.author.id
-        # user = await client.fetch_user(user_id)
-        # user = <class 'discord.user.User'>
-        logger.info("Voice print by: " + str(message.author))
-        # database(user_id, 0.0)
-
-    # Command profile
-    # if message.content.startswith("!profile"):
-    #     user_id = message.author.id
-    #     user = await client.fetch_user(user_id)
-    #     member = discord.ext.commands.MemberConverter(user)
-    #     logger.debug(member, "\n", type(member))
-    #     user = await client.fetch_user(user_id)
-    #     user = <class 'discord.user.User'>
-    #     profile = discord.Embed(
-    #         title="Профиль" + str(message.author),
-    #         description=f"""Nickname: {message.author}
-    #         id: {message.author.id}
-    #         Дата создания аккаунта: {user.created_at()}
-    #         Дата входа на сервер: """,
-    #     )
-    #     await message.channel.send(embed=profile)
-
     # Command manual
     if message.content.startswith("!manual"):
         manual = discord.Embed(
@@ -211,8 +127,7 @@ def main() -> None:
     logger.info("Version bot: " + __version__)
     logger.info("OS: " + sys.platform)
     logger.info("Python version: " + sys.version)
-    logger.info("Version dircord.py: " + discord.__version__)
-    logger.info("Version sqlite3: " + sql.sqlite_version)
+    logger.info("Version discord.py: " + discord.__version__)
     client.run(TOKEN)
 
 
